1688247c8d5cb12d400a2c93c09a8988a0cb17e0
Dajeong Park <jul897813@gmail.com>
2022-12-07 01:24:35 +0900

    first commit

---
+++ src/test/java/gachon/kakao/academy/kafkastreams/KafkaStreamsApplicationTests.java
@@ -0,0 +1,13 @@
+package gachon.kakao.academy.kafkastreams;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStreamsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
